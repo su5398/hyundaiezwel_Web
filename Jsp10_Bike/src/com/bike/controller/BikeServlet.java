8c522a12f9b457ccd7a3223ef20087158a6a263c
Kweon <su539@naver.com>
2024-10-14 17:36:48 +0900

    Web_Jsp_Tomcat_Bike

---
+++ Jsp10_Bike/src/com/bike/controller/BikeServlet.java
@@ -0,0 +1,30 @@
+package com.bike.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/bike")
+public class BikeServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html; charset=UTF-8");
+		
+		String command = request.getParameter("command");
+		System.out.println("command: "+command);
+		
+		if(command.equals("first")) {
+			response.sendRedirect("bike01.jsp");
+		}
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
