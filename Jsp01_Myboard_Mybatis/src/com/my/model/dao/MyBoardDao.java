6cf70021d4e7ea496d1f919cdf16085fbd8e8ad9
Kweon <su539@naver.com>
2024-10-16 15:16:44 +0900

    Web_Jsp_Tomcat_MyBoard_MyBatis

--- Jsp01_Myboard_Mybatis/src/com/my/model/dao/MyBoardDao.java
+++ Jsp01_Myboard_Mybatis/src/com/my/model/dao/MyBoardDao.java
@@ -1,29 +1,45 @@
 package com.my.model.dao;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.ibatis.session.SqlSession;
+
 import com.my.model.dto.MyBoard;
 
-public class MyBoardDao {
+public class MyBoardDao extends SqlMapConfig{
 	
 	public List<MyBoard> selectAll(){
+		List<MyBoard> res = new ArrayList<>();
+		SqlSession session = null;
 		
-		return null;
+		session = getSqlSessionFactory().openSession(true);
+		res = session.selectList("com.my.myboard.selectAll");
+		
+		session.close();
+		
+		return res;
 	}
 	//선택출력
 	public MyBoard selectOne(int myno) {
+		SqlSession session = null;
+		MyBoard res = null;
 		
-		return null;
+		
+		try {
+			session = getSqlSessionFactory().openSession(true);
+			res = session.selectOne("com.my.myboard.selectOne",myno);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			session.close();
+		}
+		
+		return res;
 	}
 	//추가
 	public int insert(MyBoard dto) {
+		//힌트: ("namespace.id",dto)
 		
 		return 0;
 	}
