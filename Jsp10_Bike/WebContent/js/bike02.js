0f511b12c7b3169a5064e13b5cca15cdb96380cf
Kweon <su539@naver.com>
2024-10-16 09:47:41 +0900

    Web_Jsp_Tomcat_Bike

--- Jsp10_Bike/WebContent/js/bike02.js
+++ Jsp10_Bike/WebContent/js/bike02.js
@@ -14,6 +14,39 @@ function parseJson(){
 			data: {"obj":JSON.stringify(data)},
 			success:function(msg){
 				alert(msg);
+				if(msg>0){
+					$.each(data, function(key,val){
+						if(key == "DESCRIPTION"){
+							$("thead").append(
+								"<tr>"+
+									"<th>"+val.ADDR_GU+"</th>"+
+									"<th>"+val.CONTENT_ID+"</th>"+
+									"<th>"+val.CONTENT_NM+"</th>"+
+									"<th>"+val.NEW_ADDR+"</th>"+
+									"<th>"+val.CRADLE_COUNT+"</th>"+
+									"<th>"+val.LONGITUDE+"</th>"+
+									"<th>"+val.LATITUDE+"</th>"+
+								"</tr>");
+						}else{
+							for(let i=0; i<val.length; i++){
+								let str = val[i];
+								
+								$("tbody").append(
+									"<tr>"+
+										"<td>"+str.addr_gu+"</td>"+
+										"<td>"+str.content_id+"</td>"+
+										"<td>"+str.content_nm+"</td>"+
+										"<td>"+str.new_addr+"</td>"+
+										"<td>"+str.cradle_count+"</td>"+
+										"<td>"+str.longitude+"</td>"+
+										"<td>"+str.latitude+"</td>"+
+									"</tr>");
+								
+								
+							}
+						}
+					});
+				}
 			},
 			error:function(){
 				alert("data 저장 실패!");
